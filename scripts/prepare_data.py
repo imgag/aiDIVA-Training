2dbdb462f7987edf2c8a4c79987723bcdda84ffc
Dominic Boceck <dboceck@gmail.com>
2020-06-11 19:16:08 +0200

    change used feature set

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -8,9 +8,10 @@ random_seed = 14038
 
 ## customize this method according to the features used in the training
 def prepare_data_and_fill_missing_values(data_to_prepare, feature_list):
+    # make sure all population allele frequencies are present in the input file
     data_to_prepare["gnomAD_AF"] = data_to_prepare["gnomAD_AF"].fillna(0)
     data_to_prepare["gnomAD_AFR_AF"] = data_to_prepare["gnomAD_AFR_AF"].fillna(0)
-    data_to_prepare["gnomAD_AMF_AF"] = data_to_prepare["gnomAD_AMR_AF"].fillna(0)
+    data_to_prepare["gnomAD_AMR_AF"] = data_to_prepare["gnomAD_AMR_AF"].fillna(0)
     data_to_prepare["gnomAD_ASJ_AF"] = data_to_prepare["gnomAD_ASJ_AF"].fillna(0)
     data_to_prepare["gnomAD_EAS_AF"] = data_to_prepare["gnomAD_EAS_AF"].fillna(0)
     data_to_prepare["gnomAD_FIN_AF"] = data_to_prepare["gnomAD_FIN_AF"].fillna(0)
@@ -29,7 +30,7 @@ def prepare_data_and_fill_missing_values(data_to_prepare, feature_list):
 
     data_to_prepare["gnomAD_AF"] = data_to_prepare["gnomAD_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
     data_to_prepare["gnomAD_AFR_AF"] = data_to_prepare["gnomAD_AFR_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
-    data_to_prepare["gnomAD_AMF_AF"] = data_to_prepare["gnomAD_AMR_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
+    data_to_prepare["gnomAD_AMR_AF"] = data_to_prepare["gnomAD_AMR_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
     data_to_prepare["gnomAD_ASJ_AF"] = data_to_prepare["gnomAD_ASJ_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
     data_to_prepare["gnomAD_EAS_AF"] = data_to_prepare["gnomAD_EAS_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
     data_to_prepare["gnomAD_FIN_AF"] = data_to_prepare["gnomAD_FIN_AF"].apply(lambda row: max([float(value) for value in str(row).split("&")]))
@@ -49,15 +50,27 @@ def prepare_data_and_fill_missing_values(data_to_prepare, feature_list):
     if not "MaxAF" in data_to_prepare.columns:
         data_to_prepare[["MaxAF"]] = data_to_prepare.apply(lambda row: pd.Series(max([float(row["AFR_AF"]), float(row["AMR_AF"]), float(row["EAS_AF"]), float(row["EUR_AF"]), float(row["SAS_AF"]), float(row["AA_AF"]), float(row["EA_AF"]), float(row["gnomAD_AFR_AF"]), float(row["gnomAD_AMR_AF"]), float(row["gnomAD_ASJ_AF"]), float(row["gnomAD_EAS_AF"]), float(row["gnomAD_FIN_AF"]), float(row["gnomAD_NFE_AF"]), float(row["gnomAD_OTH_AF"]), float(row["gnomAD_SAS_AF"])])), axis=1)
 
-    data_to_prepare["PROVEAN_score"] = data_to_prepare["PROVEAN_score"].apply(lambda row: min([float(value) for value in str(row).split("&") if value != "."]))
-    data_to_prepare["Polyphen2_HDIV_score"] = data_to_prepare["Polyphen2_HDIV_score"].apply(lambda row: max([float(value) for value in str(row).split("&") if value != "."]))
-    data_to_prepare["Polyphen2_HVAR_score"] = data_to_prepare["Polyphen2_HVAR_score"].apply(lambda row: max([float(value) for value in str(row).split("&") if value != "."]))
-    data_to_prepare["VEST"] = data_to_prepare["VEST"].apply(lambda row: max([float(value) for value in str(row).split("&") if value != "."]))
-    data_to_prepare["fathmm_score"] = data_to_prepare["fathmm_score"].apply(lambda row: min([float(value) for value in str(row).split("&") if value != "."]))
+    data_to_prepare["SIFT"] = data_to_prepare["SIFT"].apply(lambda row: min([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["PolyPhen"] = data_to_prepare["PolyPhen"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["Condel"] = data_to_prepare["Condel"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["CADD_PHRED"] = data_to_prepare["CADD_PHRED"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["CADD_RAW"] = data_to_prepare["CADD_RAW"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["REVEL"] = data_to_prepare["REVEL"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["segmentDuplication"] = data_to_prepare["segmentDuplication"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    #data_to_prepare["simpleRepeat"] = data_to_prepare["simpleRepeat"].apply(lambda row: max([float(value) for value in str(row).split("&") if (value != "." & value != "nan")]))
+    #data_to_prepare["custom_EIGEN_PHRED"] = data_to_prepare["custom_EIGEN_PHRED"].apply(lambda row: max([float(value) for value in str(row).split("&") if (value != "." & value != "nan")]))
+    data_to_prepare["custom_SIFT"] = data_to_prepare["custom_SIFT"].apply(lambda row: min([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["fannsdb_CONDEL"] = data_to_prepare["fannsdb_CONDEL"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["fannsdb_FATHMM"] = data_to_prepare["fannsdb_FATHMM"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["fannsdb_MA"] = data_to_prepare["fannsdb_MA"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["fannsdb_POLYPHEN2"] = data_to_prepare["fannsdb_POLYPHEN2"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["fannsdb_SIFT"] = data_to_prepare["fannsdb_SIFT"].apply(lambda row: min([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["custom_MutationAssessor"] = data_to_prepare["custom_MutationAssessor"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
+    data_to_prepare["custom_FATHMM_XF"] = data_to_prepare["custom_FATHMM_XF"].apply(lambda row: max([float(value) for value in str(row).split("&") if ((value != ".") & (value != "nan"))], default=np.nan))
 
     # make sure that the following three parameters are named correctly if they are present in the feature_list
     for feature in feature_list:
-        if feature == "SegDupMax":
+        if feature == "segmentDuplication":
             data_to_prepare[feature].fillna(0, inplace=True)
         elif feature == "MaxAF":
             data_to_prepare[feature].fillna(0, inplace=True)
@@ -80,7 +93,9 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--in_data', type=str, dest='in_data', metavar='input.csv', required=True, help='CSV file containing the data that should be prepared\n')
     parser.add_argument('--out_data', type=str, dest='out_data', metavar='outpu.csv', required=True, help='CSV file where the prepared data is stored\n')
-    parser.add_argument('--feature_list', type=str, dest='feature_list', required=True, help='List containing the features that should be prepared\n')
+    parser.add_argument('--feature_list', type=str, dest='feature_list', metavar='feature_name1,feaute_name2,feature_name3', required=True, help='List containing the used feature names separated with a comma\n')
     args = parser.parse_args()
 
-    perform_preparation(args.in_data, args.out_data, args.feature_list)
+    feature_list = args.feature_list.split(",")
+
+    perform_preparation_and_save(args.in_data, args.out_data, feature_list)
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -8,6 +8,12 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import matthews_corrcoef
 
 
 random_seed = 14038
@@ -21,15 +27,13 @@ def read_train_and_test_data(train_data_file, test_data_file):
 
 
 def extract_features_from_input_data(data_to_extract, feature_list):
-    prepared_labels = np.asarray(data_to_prepare["RANK"])
-    prepared_features = np.asarray(data_to_prepare[feature_list])
+    extracted_labels = np.asarray(data_to_extract["RANK"])
+    extracted_features = np.asarray(data_to_extract[feature_list])
 
-    return prepared_features, prepared_labels
+    return extracted_features, extracted_labels
 
 
-def train_model_with_gridsearch(training_data):
-    extract_features_from_training_data
-
+def train_model_with_gridsearch(training_features, training_labels):
     # grid with the default parameters
     parameter_grid = {"n_estimators": [1000],
                       "criterion": ["gini"],
@@ -52,9 +56,17 @@ def train_model_with_gridsearch(training_data):
 
 
 def compute_model_statistics(trained_model, test_features, test_labels):
+    prediction = trained_model.predict(test_features)
+    
     print("Parameters of the trained model: \n")
     pprint(trained_model.get_params())
-    print("Mean accuracy: ", trained_model.score(test_features, test_labels))
+    print("Mean accuracy: ", trained_model.score(test_features, test_labels))    
+    print("Accuracy: ", accuracy_score(test_labels, prediction))
+    print("Balanced-Accuracy: ", balanced_accuracy_score(test_labels, prediction))
+    print("Precision: ", precision_score(test_labels, prediction))
+    print("Recall: ", recall_score(test_labels, prediction))
+    print("Average-Precision: ", average_precision_score(test_labels, prediction))
+    print("Matthews Correlation Coefficient: ", matthews_corrcoef(test_labels, prediction))
 
 
 def export_trained_model(export_file, model_to_export):
@@ -64,7 +76,7 @@ def export_trained_model(export_file, model_to_export):
     pickle.dump(model_to_export, open(export_file, 'wb'))
 
 
-def perform_model_training_and_evaluation(feature_list, training_data, test_data=None, model_name):
+def perform_model_training_and_evaluation(feature_list, training_data, test_data, model_name):
     training_features, training_labels = extract_features_from_input_data(training_data, feature_list)
 
     trained_rf_model = train_model_with_gridsearch(training_features, training_labels)
@@ -80,7 +92,11 @@ if __name__=='__main__':
     parser.add_argument('--train_data', type=str, dest='train_data', metavar='train.csv', required=True, help='CSV file containing the training data, used to train the random forest model\n')
     parser.add_argument('--test_data', type=str, dest='test_data', metavar='tets.csv', required=True, help='CSV file containing the test data, used to compute the model statistics\n')
     parser.add_argument('--model_name', type=str, dest='model_name', metavar='model.pkl', required=True, help='Specifies the name of the trained model to export\n')
-    parser.add_argument('--feature_list', type=str, dest='feature_list', required=True, help='List containing the features that should be used in the training\n')
+    parser.add_argument('--feature_list', type=str, dest='feature_list', metavar='feature1,feature2,feature3', required=True, help='List containing the features that should be used in the training\n')
     args = parser.parse_args()
 
-    perform_model_training_and_evaluation(args.feature_list, args.train_data, args.test_data, args.model_name)
+    train_data = pd.read_csv(args.train_data, sep="\t", low_memory=False)
+    test_data = pd.read_csv(args.test_data, sep="\t", low_memory=False)
+    feature_list = args.feature_list.split(",")
+
+    perform_model_training_and_evaluation(feature_list, train_data, test_data, args.model_name)
